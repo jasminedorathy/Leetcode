ea7eb2e17e555c807d652f1bafc0a0194a7d58c1
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-01-21 16:21:24 +0530

    Time: 10 ms (11.44%), Space: 40.3 MB (62.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,9 @@
 class Solution {
     public int fib(int n) {
-       if(n<2)
-       {
-        return n;
-       }
-       
-    int [] d = {0,0,1};
-    for(int i = 2;i<=n;i++)
-    {
-        d[0] = d[1];
-        d[1] = d[2];
-        d[2] = d[0] + d[1];
-    }
-
-    return d[2];
-
+        if(n==0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else return fib(n-1) + fib(n-2);
     }
 }
