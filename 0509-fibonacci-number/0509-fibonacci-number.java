9d01c0a83a974d398ad6564ed99d043ccb473ec3
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-01-21 16:24:53 +0530

    Time: 0 ms (100%), Space: 40.8 MB (17.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,20 @@
 class Solution {
     public int fib(int n) {
+        int a = 0;
+        int b = 1;
         if(n==0)
+        {
             return 0;
-        else if(n == 1)
-            return 1;
-        else return fib(n-1) + fib(n-2);
+        }
+        else{
+        for (int i=2;i<=n;i++)
+        {
+            int c = b;
+            b = b+a;
+            a=c;
+        }
+        return b;
+        }
+        
     }
 }
