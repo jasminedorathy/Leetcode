fbe8b9abb9915270362507b078a8877faef3d7d4
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-01-25 16:20:58 +0530

    Time: 1 ms (29.14%), Space: 41.9 MB (97.43%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,28 @@
  class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Append elements from nums2 into nums1 starting at index m
+        
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
         }
-        // Sort the combined array
+        
         Arrays.sort(nums1);
     }
 }
+
+/*
+class Main1
+{
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        
+        Arrays.sort(nums1);
+        for(int i:nums1)
+        {
+            System.out.println(i);
+        }
+    }
+}
+*/
