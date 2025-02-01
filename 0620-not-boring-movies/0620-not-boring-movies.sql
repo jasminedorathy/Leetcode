b4166e37d5f52c669aa473bd9057c7b6324da771
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-02-01 19:49:14 +0530

    Time: 402 ms (50.03%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,4 @@
-SELECT * FROM Cinema WHERE MOD(id , 2) = 1 AND
-description <> 'boring' ORDER BY rating DESC
+SELECT id, movie, description, rating
+FROM Cinema
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
