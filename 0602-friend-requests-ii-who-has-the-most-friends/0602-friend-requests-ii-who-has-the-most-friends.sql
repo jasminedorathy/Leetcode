983d1e902a17ed82c162a75c66020eed7b41f67a
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-02-01 20:14:53 +0530

    Time: 1086 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,8 @@
+with cte(id) as (select requester_id from RequestAccepted 
+UNION ALL 
+select accepter_id from RequestAccepted
+)
+select  distinct id,
+count(id) over (partition by id) as num
+from cte
+order by num desc limit 1;
