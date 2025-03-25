eac6ee9e90b50f48bda72d6a2a4910c8a02a688d
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2025-03-25 19:18:24 +0530

    Time: 1 ms (100%), Space: 42.7 MB (86.3%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.java
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.java
@@ -1,17 +1,21 @@
 class Solution {
+    private int num(int n){
+        int max = 0;
+        while(n > 0)
+        {
+            int rem = n%10;
+            max =  Math.max(max,rem);
+            n /= 10;
+        }
+        return max;
+    }
     public int sumOfEncryptedInt(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]<=9) sum += nums[i];
-            else if(nums[i]<=99) {
-                nums[i] = nums[i]/10;
-                sum += nums[i]*11;
-            }
-             else if(nums[i]<=999) {
-                nums[i] = nums[i]/100;
-                sum += nums[i]*111;
-            }
+            else if(nums[i]<=99) sum += num(nums[i])*11;
+            else if(nums[i]<=999) sum += num(nums[i])*111;
             else sum += 1111;
         }
         return sum;
