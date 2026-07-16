e06ce3f9f5d7c8348c47f51e82656b6884c738c7
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-07-16 14:57:13 +0530

    Time: 229 ms (97.47%), Space: 0B (100%) - LeetHub

--- 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-update salary set sex = char(211 - ascii(sex));
+update salary set sex = 
+case sex 
+when 'm' then 'f'
+else 'm'
+end;
+
+-- char(211 - ascii(sex));
