8c8a119cf6dec40cd9fd7157bab14df4df2a9440
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-08-22 12:14:33 +0530

    Time: 624 ms (79.94%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,16 +1,9 @@
--- Write your MySQL query statement below
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016 
-FROM Insurance
-WHERE 
-    tiv_2015 IN (
-        SELECT tiv_2015
-        FROM Insurance
-        GROUP BY tiv_2015
-        HAVING COUNT(*) > 1
-    )
-    AND (lat, lon) IN (
-        SELECT lat, lon
-        FROM Insurance
-        GROUP BY lat, lon
-        HAVING COUNT(*) = 1
-    );
+/* Write your PL/SQL query statement below */
+with temp1 as(select tiv_2015 from insurance group by tiv_2015 having count(*) > 1),
+temp2 as (select lat,lon from insurance group by lat,lon having count(*) = 1)
+select round(sum(i.tiv_2016),2) as tiv_2016 from
+insurance i
+join temp1 t1 on
+i.tiv_2015 = t1.tiv_2015
+join temp2 t2 on 
+i.lat = t2.lat and i.lon = t2.lon; 
