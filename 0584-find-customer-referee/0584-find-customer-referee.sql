5900aef3fddf3e01f19fc8fe2e248dd78b896d6b
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-08-22 11:54:34 +0530

    Time: 484 ms (83.97%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,2 @@
-# Write your MySQL query statement below
-Select name
-from customer
-where coalesce(referee_id,"") != 2 
+/* Write your PL/SQL query statement below */
+select name from customer where referee_id != 2 or referee_id is null;
