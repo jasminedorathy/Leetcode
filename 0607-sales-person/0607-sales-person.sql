583ba9716cf4f784473345c207eb7ddb1b264ea4
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-08-23 16:42:33 +0530

    Time: 1862 ms (7.86%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,17 +1,11 @@
-WITH temp1 AS (
-    SELECT com_id
-    FROM Company
-    WHERE name = 'RED'
-),
-temp2 AS (
-    SELECT DISTINCT o.sales_id
-    FROM Orders o
-    JOIN temp1 t
-        ON o.com_id = t.com_id
-)
-SELECT s.name
-FROM SalesPerson s
-WHERE s.sales_id NOT IN (
-    SELECT sales_id
-    FROM temp2
+WITH cte AS(
+    select o.sales_id
+    from orders o 
+    join company c 
+    on c.com_id = o.com_id and c.name = 'RED'
+) 
+select name 
+from SalesPerson 
+where sales_id not in (
+    select sales_id from cte
 );
