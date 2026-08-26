a20c34338e7912cb570c323f529458283a744d6c
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-08-27 00:59:24 +0530

    Time: 543 ms (10.58%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,8 @@
-
-select MAX(salary) as SecondHighestSalary from
-(select salary,dense_rank() over(order by salary desc) as rnk from employee) temp
+select max(salary) as  SecondHighestSalary from 
+(select salary, dense_rank() over(order by salary desc)as rnk from employee) temp
 where rnk = 2;
+
+
+-- select MAX(salary) as SecondHighestSalary from
+-- (select salary,dense_rank() over(order by salary desc) as rnk from employee) temp
+-- where rnk = 2;
