940ccab6fd27b7673340e75c8780312cdc63372f
Jasmine Dorathy <144591240+jasminedorathy@users.noreply.github.com>
2026-08-27 01:03:09 +0530

    Time: 374 ms (78.95%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,3 +1,3 @@
 /* Write your PL/SQL query statement below */
-select score,rank from (select score,dense_rank() over (order by score desc) as rank 
-from scores);
+select score,rank from (select score,dense_rank() over(order by score desc) as rank from scores ) ;
+
